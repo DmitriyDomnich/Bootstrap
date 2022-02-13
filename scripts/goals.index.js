030b7bc8e61d6876aeceef54ea619b6fcca8417d
Dmytro <d.domnich2017@gmail.com>
2022-02-13 03:01:40 +0200

    made goals page

---
+++ scripts/goals.index.js
@@ -0,0 +1,42 @@
+const searchInputEl = document.getElementById('search-input');
+
+let searchCollapsed = true;
+
+window.onresize = resizeEvent => {
+  if (window.innerWidth > 768) {
+    showSearchBar(true);
+  } else {
+    showSearchBar();
+  }
+  searchCollapsed = true;
+};
+
+
+document.getElementById('search-button').onclick = clickEvent => {
+  if (window.innerWidth >= 768) return;
+
+  if (searchCollapsed) {
+    showSearchBar(true);
+    searchInputEl.focus();
+  } else {
+    showSearchBar();
+  }
+  searchCollapsed = !searchCollapsed;
+};
+
+function showSearchBar(show = false) {
+  if (show) {
+    searchInputEl.style.cssText = `
+      width: 1%;
+      height: calc(1.5em + .75rem + 2px);
+      padding: .375rem .75rem;
+    `;
+  } else {
+    searchInputEl.style.cssText = `
+      padding: 0;
+      width: 0;
+      height: 0;
+      border: 0;
+    `;
+  }
+}
