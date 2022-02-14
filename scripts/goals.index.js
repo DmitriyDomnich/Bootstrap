c6e84136a2be260cd9340af8fb32373b9784976d
Dmytro <d.domnich2017@gmail.com>
2022-02-14 12:19:42 +0200

    fixed smth

--- scripts/goals.index.js
+++ scripts/goals.index.js
@@ -2,51 +2,17 @@ const searchInputEl = document.getElementById('search-input');
 
 let searchCollapsed = true;
 
-window.onresize = resizeEvent => {
+window.onresize = ev => {
   if (window.innerWidth > 768) {
     showSearchBar(true);
   } else {
     showSearchBar();
   }
   searchCollapsed = true;
+  document.querySelectorAll('.my-popover').forEach(el => el.remove());
+  document.querySelectorAll('[data-toggle="popover"]')
+    .forEach(el => el.addEventListener('mouseenter', onPopoverMouseover));
 };
-
-document.querySelectorAll('[data-toggle="popover"]').forEach(el => {
-  el.addEventListener('mouseenter', onPopoverMouseover);
-});
-
-function onPopoverMouseover(ev) {
-  const el = ev.target;
-  el.removeEventListener('mouseenter', onPopoverMouseover);
-  const coords = el.getBoundingClientRect();
-  
-
-  const popover = document.createElement('div');
-  popover.classList.add('px-4', 'py-3', 'border', 'bg-light',
-   'rounded', 'shadow', 'position-absolute', 'my-popover');
-  popover.innerText = el.title;
-  popover.style.top = coords.bottom + window.scrollY + 5 + 'px';
-  document.body.append(popover);
-
-  const closeBtn = document.createElement('button');
-  closeBtn.type = 'button';
-  closeBtn.style.display = 'inline-block';
-  closeBtn.innerHTML = '<i class="bi bi-x"></i>';
-  closeBtn.classList.add('position-absolute', 'border-0');
-  closeBtn.style.cssText = `
-    right: 0;
-    top: .25rem;
-    font-size: .8em;
-  `;
-
-  closeBtn.addEventListener('click', (ev) => {
-    popover.remove();
-    el.addEventListener('mouseenter', onPopoverMouseover);
-  });
-  popover.append(closeBtn);
-
-  popover.style.left = coords.left + coords.width / 2 - popover.clientWidth / 2 + 'px';
-}
 
 document.getElementById('search-button').onclick = clickEvent => {
   if (window.innerWidth >= 768) return;
@@ -59,6 +25,56 @@ document.getElementById('search-button').onclick = clickEvent => {
   }
   searchCollapsed = !searchCollapsed;
 };
+
+document.querySelectorAll('[data-toggle="popover"]')
+  .forEach(el => el.addEventListener('mouseenter', onPopoverMouseover));
+
+function onPopoverMouseover(ev) {
+  const el = ev.target;
+
+  el.removeEventListener('mouseenter', onPopoverMouseover);
+  
+  const coords = el.getBoundingClientRect();
+
+  const popover = getPopover(el);
+  popover.style.top = coords.bottom + window.scrollY + 5 + 'px';
+
+  document.body.append(popover);
+  popover.style.left = coords.left + coords.width / 2 - popover.clientWidth / 2 + 'px';
+}
+
+
+function getPopover(el) {
+  const popover = document.createElement('div');
+  popover.classList.add('px-4', 'py-3', 'border', 'bg-light',
+   'rounded', 'shadow', 'position-absolute', 'my-popover');
+  popover.innerHTML = el.ariaLabel;
+
+  const closeBtn = getClosePopoverButton();
+
+  closeBtn.addEventListener('click', ev => {
+    popover.remove();
+    el.addEventListener('mouseenter', onPopoverMouseover);
+  });
+
+  popover.append(closeBtn);
+  return popover;
+}
+function getClosePopoverButton() {
+  const closeBtn = document.createElement('button');
+
+  closeBtn.type = 'button';
+  closeBtn.innerHTML = '<i class="bi bi-x"></i>';
+  closeBtn.style.cssText = `
+    right: 0;
+    top: .25rem;
+    font-size: .8em;
+    border: none;
+    position: absolute;
+  `;
+
+  return closeBtn;
+}
 
 function showSearchBar(show = false) {
   if (show) {
