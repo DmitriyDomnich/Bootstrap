f447e4e663bd92faa65aa81c3272aeabd8a527e7
Dmytro <d.domnich2017@gmail.com>
2022-02-14 02:16:41 +0200

    finished

--- scripts/goals.index.js
+++ scripts/goals.index.js
@@ -11,6 +11,41 @@ window.onresize = resizeEvent => {
   searchCollapsed = true;
 };
 
+document.querySelectorAll('[data-toggle="popover"]').forEach(el => {
+  el.addEventListener('mouseenter', onPopoverMouseover);
+});
+
+function onPopoverMouseover(ev) {
+  const el = ev.target;
+  el.removeEventListener('mouseenter', onPopoverMouseover);
+  const coords = el.getBoundingClientRect();
+  
+
+  const popover = document.createElement('div');
+  popover.classList.add('px-4', 'py-3', 'border', 'bg-light', 'rounded', 'shadow', 'position-absolute');
+  popover.innerText = el.title;
+  popover.style.top = coords.bottom + window.scrollY + 5 + 'px';
+  document.body.append(popover);
+
+  const closeBtn = document.createElement('button');
+  closeBtn.type = 'button';
+  closeBtn.style.display = 'inline-block';
+  closeBtn.innerHTML = '<i class="bi bi-x"></i>';
+  closeBtn.classList.add('position-absolute', 'border-0');
+  closeBtn.style.cssText = `
+    right: 0;
+    top: .25rem;
+    font-size: .8em;
+  `;
+
+  closeBtn.addEventListener('click', (ev) => {
+    popover.remove();
+    el.addEventListener('mouseenter', onPopoverMouseover);
+  });
+  popover.append(closeBtn);
+
+  popover.style.left = coords.left + coords.width / 2 - popover.clientWidth / 2 + 'px';
+}
 
 document.getElementById('search-button').onclick = clickEvent => {
   if (window.innerWidth >= 768) return;
